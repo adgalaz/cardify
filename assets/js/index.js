4c665ce369c6d198e0d37a2b0559d023007e74c2
SabrinaVillalobos <wafflish@gmail.com>
2018-01-20 22:42:20 -0300

    caption code

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,4 +8,19 @@ $(document).ready(function() {
 var addTags = (function() {
   $('img').wrap('<figure></figure>');
   $('<figcaption></figcaption>').insertAfter('img');
+
+
+/*
+* envuelve las imagenes en un elemento figure
+*/
+ $("img").wrap('<figure class="alt-text"/>');
+ /*
+ *agrega el texto del alt en un figcaption
+ */
+ $("img").each(function() {
+  $(this).after('<figcaption class="alt">' + $(this).attr('alt') + '</figcaption>');
+  })
+
+
+
 });
