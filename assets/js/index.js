4db8ed12c5ed340de603945b817cdb2e3427746b
SabrinaVillalobos <wafflish@gmail.com>
2018-02-01 18:51:34 -0300

    demo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,36 @@
 $(document).ready(function() {
-  $('.imgwrap').cardify({});
+  $('.cardifyDown').cardify({});
 });
 
 (function ($){
-  $.fn.cardify = function() {
+  $.fn.cardifyDown = function() {
 /*
 * envuelve las imagenes en un elemento figure
 */
-$('img').wrap('<figure class="alt-text"/>');
+$('.cardifyDown').wrap('<figure class="alt-text"/>');
 /*
  *agrega el texto del alt en un figcaption
  */
-  $('img').each(function() {
-    $(this).after('<figcaption class="alt">' + $(this).attr('alt') + '</figcaption>');
+  $('.cardifyDown').each(function() {
+    $(this).after('<figcaption class="down">' + $(this).attr('alt') + '</figcaption>');
   });
-
   };
+
+
+
+
+function cardifyUp(){
+/*
+* envuelve las imagenes en un elemento figure
+*/
+$('.cardifyUp').wrap('<figure class="alt-text"/>');
+/*
+ *agrega el texto del alt en un figcaption
+ */
+  $('.cardifyUp').each(function() {
+    $(this).after('<figcaption class="up">' + $(this).attr('alt') + '</figcaption>');
+  });
+}
+
 
 })($);
