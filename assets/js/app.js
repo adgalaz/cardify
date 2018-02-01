89525b76bdf1bec1270f16383064fb0227f77c80
adgalaz <adgalaz@gmail.com>
2018-02-01 12:52:20 -0300

    subiendo cambios al html, js y readme

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.step1').text('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>');
+  $('#step2').text('<link rel="stylesheet" href="path-to-cardify.css">');
+  $('#step3').text('<link rel="stylesheet" href="path-to-cardify.js">');
+  $('#step4').text('<div class="imgwrap"><img src="path-to-image.jpg" alt="my image"></div>');
+  $('#step5').text('<link rel="stylesheet" href="https://cdn.rawgit.com/adgalaz/cardify/heidi/assets/css/cardify.css">');
+  $('#step6').text('<script src="https://cdn.rawgit.com/adgalaz/cardify/heidi/src/cardify.js"></script>');
+});
